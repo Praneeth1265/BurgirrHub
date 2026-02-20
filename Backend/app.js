f7e684684b17d90ba9fdacea84c52285db021fed
Praneeth Gourabattuni <gpraneeth555@gmail.com>
2026-02-20 09:36:34 +0530

    Updated backend

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://burgirrhub.vercel.app",
     methods: ["POST", "GET", "PUT","DELETE"],
     credentials: true,
   })
