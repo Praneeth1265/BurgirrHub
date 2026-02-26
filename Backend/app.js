92fae1c95a0a83dddb0984e2ede5c9444574aefe
Praneeth Gourabattuni <gpraneeth555@gmail.com>
2026-02-26 09:50:17 +0530

    Resolved an issue with CORS

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://burgirrhub.vercel.app",
+    origin: "https://burgirr-hub-ew3o.vercel.app",
     methods: ["POST", "GET", "PUT","DELETE"],
     credentials: true,
   })
